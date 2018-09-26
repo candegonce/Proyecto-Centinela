6276189e3a7d3bff0937089244fd0a02b823f53a
candegonce <candegonce@hotmail.com>
2018-09-25 23:52:46 -0300

    add limit mediciones

--- CentinelaFrontEnd/src/app/modelos/medicion.ts
+++
@@ -1,9 +0,0 @@
-import { Sensor } from './sensor';
-export class Medicion {
-  idMedicion:number;
-  dispositivo:Sensor;
-  dioxidoCarbono:number;
-  temperatura:number;
-  fecha:Date; 
-  constructor(){}
-}
--- CentinelaFrontEnd/src/app/modelos/ubicacion.ts
+++
@@ -1,9 +0,0 @@
-export class Ubicacion {
-  idUbicacion:number;
-  calle:string;  
-  numero:number;  
-  localidad:string;  
-  longitud:string;   
-  latitud:string;
-  constructor(){}
-}
